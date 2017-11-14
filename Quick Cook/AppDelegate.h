8558513eb19fd48f37fd7520ff71d554435e3f3c
Augustus Boadu <my24business@gmail.com>
2017-11-14 12:50:12 +0000

    Initial Commit

---
+++ Quick Cook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quick Cook
+//
+//  Created by Augustus Boadu on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quick Cook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quick Cook
+//
+//  Created by Augustus Boadu on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
