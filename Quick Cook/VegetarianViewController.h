b9034a93e108aa985bc0ee8a5dc00767c0a055cd
Augustus Boadu <my24business@gmail.com>
2017-11-22 11:06:21 +0000

    Format changed to include tabbed views for Breakfast, Lunch, Dinner and Vegetarian. I've also added images for the dinner tab. The Launch view was updated to include an image, app title and a strapline for the author.

---
+++ Quick Cook/VegetarianViewController.h
@@ -0,0 +1,13 @@
+//
+//  VegetarianViewController.h
+//  Quick Cook
+//
+//  Created by Augustus Boadu on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VegetarianViewController : UIViewController
+
+@end
